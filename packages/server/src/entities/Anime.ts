0d6062da764c6dc5c3153b09bcca5a4be3918b82
HallexCosta <hallex.costa@hotmail.com>
2020-12-13 03:27:18 -0300

    Update entity Anime

--- packages/server/src/entities/Anime.ts
+++ packages/server/src/entities/Anime.ts
@@ -1,3 +1,23 @@
-import { AnimeTextData } from './AnimeTextData'
+type Episode = {
+  title: string
+  thumbnail: string
+  qualityStreaming: string
+  url: string
+}
 
-export type AnimeDetail = AnimeTextData
+export type Streamings = {
+  episodes: Episode[]
+  ovas: Episode[]
+}
+
+export type Anime = {
+  name: string
+  imageURL: string
+  genre: string
+  status: string
+  studio: string
+  sinopse: string
+  releaseData: number
+  rating: number
+  streamings: Streamings
+}
