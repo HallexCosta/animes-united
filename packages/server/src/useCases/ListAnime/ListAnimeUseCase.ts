e4cedb7a2bdb9834141c5439d37d7311d8c69f13
HallexCosta <hallex.costa@hotmail.com>
2020-12-14 19:56:47 -0300

    Add save log on core of use case List Anime

--- packages/server/src/useCases/ListAnime/ListAnimeUseCase.ts
+++ packages/server/src/useCases/ListAnime/ListAnimeUseCase.ts
@@ -1,10 +1,11 @@
 import { ListAnimeRequestDTO } from './ListAnimeDTO'
 import { Anime } from '@entities/Anime'
 import { IYayanimesProvider } from '@providers/IYayanimesProvider'
-import { saveFile } from '@common/utils/file'
+import { getFile, saveFile } from '@common/utils/file'
 
 import path from 'path'
 import { toUpperFirstCase } from '@common/utils/text'
+import { getCurrentDate, getCurrentTime } from '@common/utils/date'
 
 export class ListAnimeUseCase {
   constructor(private yayanimesProvider: IYayanimesProvider) {}
@@ -31,6 +32,30 @@ export class ListAnimeUseCase {
       directorySave: directoryAnimeSave,
       dataContent: anime
     })
+
+    const directoryLogSave = path.join(__dirname, '..', '..')
+
+    const currentDataContent = getFile({
+      directorySource: directoryLogSave,
+      filename: 'console',
+      extension: 'log',
+      createThisFileIfNotExists: true
+    })
+
+    const date = getCurrentDate()
+    const time = getCurrentTime()
+
+    const addNewContent = `[${date} ${time}] INFO  :...Anime ${toUpperFirstCase(
+      data.name
+    )} - Getting Successfully\n`
+
+    saveFile({
+      filename: 'console',
+      extension: 'log',
+      directorySave: directoryLogSave,
+      dataContent: `${currentDataContent}\n${addNewContent}`
+    })
+
     return anime
   }
 }
