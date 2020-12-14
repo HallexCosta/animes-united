b0cb7a30d15ecff5fa9e5abcf58a0f8858635049
HallexCosta <hallex.costa@hotmail.com>
2020-12-14 15:03:28 -0300

    Changing position of execute route animes/recommendation before animes/:name

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,15 +1,20 @@
 import { listAnimeController } from '@useCases/ListAnime'
 import { listAnimesController } from '@useCases/ListAnimes'
+import { listRecommendationAnimesController } from '@useCases/ListRecommendationAnimes'
 import { Router, Request, Response } from 'express'
 
 const routes: Router = Router()
 
-routes.get('/animes/:name', (request: Request, response: Response) => {
-  return listAnimeController.handle(request, response)
-})
-
 routes.get('/animes', (request: Request, response: Response) => {
   return listAnimesController.handle(request, response)
+})
+
+routes.get('/animes/recommendation', (request: Request, response: Response) => {
+  return listRecommendationAnimesController.handle(request, response)
+})
+
+routes.get('/animes/:name', (request: Request, response: Response) => {
+  return listAnimeController.handle(request, response)
 })
 
 export { routes }
