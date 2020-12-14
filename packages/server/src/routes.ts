c198248bfb88e712d55469ed80d3a781148db4f7
HallexCosta <hallex.costa@hotmail.com>
2020-12-14 01:21:20 -0300

    Add route for list names of animes

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,10 +1,15 @@
 import { listAnimeController } from '@useCases/ListAnime'
+import { listAnimesController } from '@useCases/ListAnimes'
 import { Router, Request, Response } from 'express'
 
 const routes: Router = Router()
 
 routes.get('/animes/:name', (request: Request, response: Response) => {
   return listAnimeController.handle(request, response)
+})
+
+routes.get('/animes', (request: Request, response: Response) => {
+  return listAnimesController.handle(request, response)
 })
 
 export { routes }
