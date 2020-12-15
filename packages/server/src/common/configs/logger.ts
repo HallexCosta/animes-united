0c38853e63c7d145510a9c641d77a2ffe10d6f26
HallexCosta <hallex.costa@hotmail.com>
2020-12-15 04:15:46 -0300

    Remove directorySave of logger config

--- packages/server/src/common/configs/logger.ts
+++ packages/server/src/common/configs/logger.ts
@@ -1,9 +1,6 @@
-import { join } from 'path'
-
 import { LoggerFileConfig } from '@common/system/Logger'
 
 export const config: LoggerFileConfig = {
   filename: 'console',
-  extension: 'log',
-  directorySource: join(__dirname, '..', '..')
+  extension: 'log'
 }
