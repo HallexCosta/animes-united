f81260a88026ef36cf5851a4fba1125af793878e
HallexCosta <hallex.costa@hotmail.com>
2020-12-15 15:42:13 -0300

    import logger

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application } from 'express'
 import { routes } from './routes'
+import { logger } from './logger'
 
 const app: Application = express()
+
+logger()
+  .then()
+  .catch(e => console.error(e))
 
 app.use(routes)
 
