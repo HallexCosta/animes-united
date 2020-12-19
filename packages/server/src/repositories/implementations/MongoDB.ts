ae82d72c6196c99ee1ca6922ab6f9a489a20d006
HallexCosta <hallex.costa@hotmail.com>
2020-12-19 01:45:39 -0300

    Initialize property collectionName

--- packages/server/src/repositories/implementations/MongoDB.ts
+++ packages/server/src/repositories/implementations/MongoDB.ts
@@ -2,7 +2,7 @@ import { Db, MongoClient } from 'mongodb'
 
 export abstract class MongoDB {
   private cachedDb: Db | null = null
-  protected collectionName?: string
+  protected collectionName = ''
 
   constructor(private uri: string) {}
 
