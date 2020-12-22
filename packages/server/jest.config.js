733de1b67a701a7d855b0b48541f0d24d5218292
HallexCosta <hallex.costa@hotmail.com>
2020-12-21 22:42:42 -0300

    Update jest.config.js of server package

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,6 +1,9 @@
+const tsconfig = require('./tsconfig.json')
 const { name } = require('./package.json')
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig)
 
 module.exports = {
   displayName: name,
-  name
+  name,
+  moduleNameMapper
 }
