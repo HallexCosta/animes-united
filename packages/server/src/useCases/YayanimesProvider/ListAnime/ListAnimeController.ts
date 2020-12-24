c1922cdc61adf4cd48f0b7442c2b6183a2082595
HallexCosta <hallex.costa@hotmail.com>
2020-12-24 02:40:01 -0300

    Remove console.error for catch Errors

--- packages/server/src/useCases/YayanimesProvider/ListAnime/ListAnimeController.ts
+++ packages/server/src/useCases/YayanimesProvider/ListAnime/ListAnimeController.ts
@@ -14,7 +14,6 @@ export class ListAnimeController {
 
       return response.status(200).json(animeTextData)
     } catch (e) {
-      console.error(e)
       return response.status(400).json({
         message: e.message || 'Unexpected error'
       })
