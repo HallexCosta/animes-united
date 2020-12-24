5745a70f796b7e22a7a76a281a1fbdc211cede45
HallexCosta <hallex.costa@hotmail.com>
2020-12-24 00:48:25 -0300

    Format in lower case all letters after first letter

--- packages/server/src/common/utils/text.spec.ts
+++ packages/server/src/common/utils/text.spec.ts
@@ -2,8 +2,8 @@ import { toUpperFirstCase } from './text'
 
 describe('Test utils/text', () => {
   it('Should be able to upper case a fisrt letter of string', () => {
-    const expected = toUpperFirstCase('charlotte')
-
-    expect(expected).toEqual('Charlotte')
+    expect(toUpperFirstCase('charlotte')).toEqual('Charlotte')
+    expect(toUpperFirstCase('cHARLOTTE')).toEqual('Charlotte')
+    expect(toUpperFirstCase('Charlotte')).toEqual('Charlotte')
   })
 })
--- packages/server/src/common/utils/text.ts
+++ packages/server/src/common/utils/text.ts
@@ -2,7 +2,7 @@ export function toUpperFirstCase(text: string) {
   let textModified = ''
 
   for (let i = 0; i < text.length; i++) {
-    textModified += i == 0 ? text[i].toUpperCase() : text[i]
+    textModified += i === 0 ? text[i].toUpperCase() : text[i].toLowerCase()
   }
 
   return textModified
