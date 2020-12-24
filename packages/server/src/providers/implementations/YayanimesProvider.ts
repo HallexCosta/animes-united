b46207a6d06a61f204b5d4437a341aa5c21942a5
HallexCosta <hallex.costa@hotmail.com>
2020-12-24 02:49:09 -0300

    Remove global error PageErrors const

--- packages/server/src/providers/implementations/YayanimesProvider.ts
+++ packages/server/src/providers/implementations/YayanimesProvider.ts
@@ -61,127 +61,115 @@ export class YayanimesProvider extends Puppeteer implements IYayanimesProvider {
       waitUntil: 'networkidle2'
     })
 
-    const errors: PageErrors = {
-      error: false
-    }
+    const anime = await page.evaluate((baseURL: string) => {
+      const verifyIsInvalidAnime = () => {
+        let error = false
 
-    const anime = await page.evaluate(
-      (baseURL: string, errors: PageErrors) => {
-        const verifyIsInvalidAnime = () => {
-          const pageNotFound = document.querySelector<HTMLHeadingElement>('h3')
-            ?.innerText
+        const pageNotFound = document.querySelector<HTMLHeadingElement>('h3')
+          ?.innerText
 
-          const episodes = [
-            ...document.querySelectorAll(
-              '.contentBox ul li > div.box-episodio3'
-            )
-          ]
+        const episodes = [
+          ...document.querySelectorAll('.contentBox ul li > div.box-episodio3')
+        ]
 
-          // Page not found
-          if (pageNotFound === 'PAGINA NÃO ENCONTRADA') {
-            console.log(`Error Message: ${pageNotFound}`)
+        // Page not found
+        if (pageNotFound === 'PAGINA NÃO ENCONTRADA') {
+          console.log(`Error Message: ${pageNotFound}`)
 
-            errors.error = true
+          error = true
+        }
+
+        // Episodes found
+        if (episodes.length <= 0) {
+          console.log('Without Episodes: ', episodes.length)
+
+          error = true
+        }
+
+        return error
+      }
+
+      const separateOvaOfEpisode = (allEpisodes: Element[]) => {
+        const episodes: Episode[] = []
+        const ovas: Episode[] = []
+
+        allEpisodes.forEach(episodeOrOva => {
+          const title = (episodeOrOva.children[0]
+            .children[0] as HTMLAnchorElement).innerText.trim()
+
+          const number = Number(
+            (episodeOrOva.children[0].children[1]
+              .children[2] as HTMLSpanElement).innerText
+              .split(' ')[1]
+              .trim()
+          )
+
+          const thumbnail = (episodeOrOva.children[0].children[1]
+            .children[0] as HTMLImageElement).src
+
+          const qualityStreaming = (episodeOrOva.children[0].children[1]
+            .children[1] as HTMLSpanElement).innerText.trim()
+
+          const route = (episodeOrOva.children[1].children[0]
+            .children[1] as HTMLAnchorElement).pathname
+
+          if (title.match(/(ova)/gi)) {
+            ovas.push({
+              title,
+              number,
+              thumbnail,
+              qualityStreaming,
+              url: `${baseURL}${route}`
+            })
+          } else {
+            episodes.push({
+              title,
+              number,
+              thumbnail,
+              qualityStreaming,
+              url: `${baseURL}${route}`
+            })
           }
+        })
 
-          // Episdodes found
-          if (episodes.length <= 0) {
-            console.log('Without Episodes: ', episodes.length)
+        return { episodes, ovas }
+      }
 
-            errors.error = true
-          }
+      if (verifyIsInvalidAnime()) {
+        return
+      }
 
-          return errors.error
+      const title = document.querySelector<HTMLSpanElement>('span.color-change')
+      const image = document.querySelector<HTMLImageElement>('#capaAnime > img')
+      const about = document.querySelectorAll<HTMLTableRowElement>(
+        'table > tbody > tr > td'
+      )
+      const sinopse = document.querySelectorAll<HTMLDivElement>('.single div')
+      const rating = document.querySelector<HTMLSpanElement>('#rmp-rating')
+
+      const allEpisodes = [...document.querySelectorAll('div.box-episodio3')]
+
+      const streamings = separateOvaOfEpisode(allEpisodes)
+
+      const anime: Anime = {
+        name: title ? title.innerText.trim() : 'Without name',
+        imageURL: image ? image.src.trim() : 'Without image',
+        studio: about[1] ? about[1].innerText.trim() : 'Without studio',
+        genre: about[3] ? about[3].innerText.trim() : 'Without genre',
+        status: about[5] ? about[5].innerText.trim() : 'Without release data',
+        yearRelease: about[7] ? Number(about[7].innerText.trim()) : 0,
+        rating: rating ? Number(rating.innerText.trim()) : 0,
+        sinopse: sinopse ? sinopse[13].innerText.trim() : 'Without sinopse',
+        streamings: {
+          episodes: streamings.episodes ? streamings.episodes.reverse() : [],
+          ovas: streamings.ovas ? streamings.ovas.reverse() : []
         }
+      }
 
-        const separateOvaOfEpisode = (allEpisodes: Element[]) => {
-          const episodes: Episode[] = []
-          const ovas: Episode[] = []
+      console.log(anime)
 
-          allEpisodes.forEach(episodeOrOva => {
-            const title = (episodeOrOva.children[0]
-              .children[0] as HTMLAnchorElement).innerText.trim()
-
-            const number = Number(
-              (episodeOrOva.children[0].children[1]
-                .children[2] as HTMLSpanElement).innerText
-                .split(' ')[1]
-                .trim()
-            )
-
-            const thumbnail = (episodeOrOva.children[0].children[1]
-              .children[0] as HTMLImageElement).src
-
-            const qualityStreaming = (episodeOrOva.children[0].children[1]
-              .children[1] as HTMLSpanElement).innerText.trim()
-
-            const route = (episodeOrOva.children[1].children[0]
-              .children[1] as HTMLAnchorElement).pathname
-
-            if (title.match(/(ova)/gi)) {
-              ovas.push({
-                title,
-                number,
-                thumbnail,
-                qualityStreaming,
-                url: `${baseURL}${route}`
-              })
-            } else {
-              episodes.push({
-                title,
-                number,
-                thumbnail,
-                qualityStreaming,
-                url: `${baseURL}${route}`
-              })
-            }
-          })
-
-          return { episodes, ovas }
-        }
-
-        if (verifyIsInvalidAnime()) {
-          return
-        }
-
-        const title = document.querySelector<HTMLSpanElement>(
-          'span.color-change'
-        )
-        const image = document.querySelector<HTMLImageElement>(
-          '#capaAnime > img'
-        )
-        const about = document.querySelectorAll<HTMLTableRowElement>(
-          'table > tbody > tr > td'
-        )
-        const sinopse = document.querySelectorAll<HTMLDivElement>('.single div')
-        const rating = document.querySelector<HTMLSpanElement>('#rmp-rating')
-
-        const allEpisodes = [...document.querySelectorAll('div.box-episodio3')]
-
-        const streamings = separateOvaOfEpisode(allEpisodes)
-
-        const anime: Anime = {
-          name: title ? title.innerText.trim() : 'Without name',
-          imageURL: image ? image.src.trim() : 'Without image',
-          studio: about[1] ? about[1].innerText.trim() : 'Without studio',
-          genre: about[3] ? about[3].innerText.trim() : 'Without genre',
-          status: about[5] ? about[5].innerText.trim() : 'Without release data',
-          yearRelease: about[7] ? Number(about[7].innerText.trim()) : 0,
-          rating: rating ? Number(rating.innerText.trim()) : 0,
-          sinopse: sinopse ? sinopse[13].innerText.trim() : 'Without sinopse',
-          streamings: {
-            episodes: streamings.episodes ? streamings.episodes.reverse() : [],
-            ovas: streamings.ovas ? streamings.ovas.reverse() : []
-          }
-        }
-
-        console.log(anime)
-
-        return anime
-      },
-      this.getBaseURL(),
-      errors
-    )
+      return anime
+    }, this.getBaseURL())
 
     await this.closePages(browser)
     await browser.close()
