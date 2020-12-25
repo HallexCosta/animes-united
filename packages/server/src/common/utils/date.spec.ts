e8fe375aabc404bb3a86613aeb2aab86b9b15358
HallexCosta <hallex.costa@hotmail.com>
2020-12-25 16:45:19 -0300

    Add test for function getCurrentDate and getCurrentTime

---
+++ packages/server/src/common/utils/date.spec.ts
@@ -0,0 +1,17 @@
+import { getCurrentDate, getCurrentTime } from './date'
+
+describe('Test utils/date', () => {
+  it('Should be able to get current date in format (yyyy-mm-dd)', () => {
+    const expected = getCurrentDate()
+
+    expect(expected).toMatch(
+      /([0-9][0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9])/g
+    )
+  })
+
+  it('Should be able to get current time in format (h:m:s)', () => {
+    const expected = getCurrentTime()
+
+    expect(expected).not.toMatch(/([0-9][0-9]):([0-9][0-9]):([0-9][0-9])/g)
+  })
+})
