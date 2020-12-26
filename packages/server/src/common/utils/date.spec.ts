3d677786775010f7a435ff9d693490cf9397ae7a
HallexCosta <hallex.costa@hotmail.com>
2020-12-26 16:27:34 -0300

    Fix separete of matching pattern getCurrentTime and getCurrentDate

--- packages/server/src/common/utils/date.spec.ts
+++ packages/server/src/common/utils/date.spec.ts
@@ -4,12 +4,12 @@ describe('Test utils/date', () => {
   it('Should be able to get current date in format (yyyy-mm-dd)', () => {
     const expected = getCurrentDate()
 
-    expect(expected).toMatch(/\b(\d+\/\d+\/\d+)\b/g)
+    expect(expected).toMatch(/\b(\d+-\d+-\d+)\b/g)
   })
 
   it('Should be able to get current time in format (h:m:s)', () => {
     const expected = getCurrentTime()
 
-    expect(expected).toMatch(/\b(\d+\/\d+\/\d+)\b/g)
+    expect(expected).toMatch(/\b(\d+:\d+:\d+)\b/g)
   })
 })
