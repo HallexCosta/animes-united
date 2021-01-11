3ded5cf5093bb7b1252f581c226110d278067f5a
HallexCosta <hallex.costa@hotmail.com>
2021-01-11 16:48:06 -0300

    Add method for separate animes names by category

--- packages/server/src/useCases/YayanimesProvider/ListAnimes/ListAnimesUseCase.ts
+++ packages/server/src/useCases/YayanimesProvider/ListAnimes/ListAnimesUseCase.ts
@@ -3,6 +3,38 @@ import { IYayanimesProvider } from '@providers/IYayanimesProvider'
 export class ListAnimesUseCase {
   public constructor(private yayanimesProvider: IYayanimesProvider) {}
 
+  private separateByCategory(names: string[]) {
+    const namesByCategories: { category: string; data: string[] }[] = []
+
+    for (const name of names) {
+      const category: string = name.match(/^[A-Z]/gi)
+        ? name[0].toUpperCase()
+        : 'Other'
+
+      const categoryFound = namesByCategories.find(
+        namesByCategory => namesByCategory.category === category
+      )
+
+      if (!categoryFound) {
+        namesByCategories.push({
+          category,
+          data: [name]
+        })
+      }
+
+      if (categoryFound) {
+        for (const namesByCategory of namesByCategories) {
+          if (namesByCategory.category === category) {
+            namesByCategory.data.push(name)
+            break
+          }
+        }
+      }
+    }
+
+    console.log(namesByCategories)
+  }
+
   public async execute(): Promise<string[]> {
     const names = await this.yayanimesProvider.getAnimeNames()
 
