7bc3b2ef229c77443abbf4ac8e9d30bf6df63723
HallexCosta <hallex.costa@hotmail.com>
2021-01-12 20:54:10 -0300

    Add component Anime

---
+++ packages/mobile/src/components/Anime/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ImageSourcePropType } from 'react-native'
+
+import { Container, Image, Title, Description } from './styles'
+
+type AnimeProps = {
+  image: ImageSourcePropType
+  title: string
+  description: string
+}
+
+export function Anime({ image, title, description }: AnimeProps): JSX.Element {
+  return (
+    <Container>
+      <Image source={image} />
+
+      <Title numberOfLines={1} ellipsizeMode="tail">
+        {title}
+      </Title>
+
+      <Description>{description}</Description>
+    </Container>
+  )
+}
---
+++ packages/mobile/src/components/Anime/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  width: 124px;
+  height: 200px;
+
+  margin-right: 15px;
+`
+
+export const Image = styled.Image`
+  width: 124px;
+  height: 160px;
+  border-radius: 8px;
+`
+
+export const Title = styled.Text`
+  color: white;
+  font-family: Nunito_700Bold;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 20px;
+
+  margin-top: 10px;
+`
+
+export const Description = styled.Text`
+  color: white;
+  font-family: Nunito_400Regular;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 14px;
+`
