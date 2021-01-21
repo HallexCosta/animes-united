2d8da88e904f2dbcb3d544e426eb0b9ded4b0649
HallexCosta <hallex.costa@hotmail.com>
2021-01-21 15:26:29 -0300

    Convert type to class Anime

--- packages/server/src/entities/Anime.ts
+++ packages/server/src/entities/Anime.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from 'mongodb'
+
 export type Episode = {
   title: string
   number: number
@@ -11,14 +13,37 @@ export type Streamings = {
   ovas: Episode[]
 }
 
-export type Anime = {
-  name: string
-  imageURL: string
-  genre: string
-  status: string
-  studio: string
-  synopsis: string
-  yearRelease: number
-  rating: number
-  streamings: Streamings
+// export type Anime = {
+//   name: string
+//   imageURL: string
+//   genre: string
+//   status: string
+//   studio: string
+//   synopsis: string
+//   yearRelease: number
+//   rating: number
+//   streamings: Streamings
+// }
+
+export class Anime {
+  public readonly id?: ObjectId
+
+  public readonly name: string
+  public readonly imageURL: string
+  public readonly genre: string
+  public readonly status: string
+  public readonly studio: string
+  public readonly synopsis: string
+  public readonly yearRelease: number
+  public readonly rating: number
+
+  public readonly streamings: Streamings
+
+  constructor(props: Omit<Anime, 'id'>, id?: ObjectId) {
+    Object.assign(this, props)
+
+    if (!id) {
+      this.id = new ObjectId()
+    }
+  }
 }
