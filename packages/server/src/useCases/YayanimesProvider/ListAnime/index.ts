c86db80a58f1fb885c6753a9f67600ba378db82f
HallexCosta <hallex.costa@hotmail.com>
2021-01-21 23:51:26 -0300

    Inject dependecy AnimeRepository on ListAnime use case

--- packages/server/src/useCases/YayanimesProvider/ListAnime/index.ts
+++ packages/server/src/useCases/YayanimesProvider/ListAnime/index.ts
@@ -1,13 +1,22 @@
+import { mongodbURI } from '@common/configs/mongodb'
 import { config } from '@common/configs/logger'
 import { Logger } from '@common/system/Logger'
 import { YayanimesProvider } from '@providers/implementations/YayanimesProvider'
+import { AnimeRepository } from '@repositories/implementations/AnimeRepository'
 import { ListAnimeController } from './ListAnimeController'
 import { ListAnimeUseCase } from './ListAnimeUseCase'
+
+const animeRepository = new AnimeRepository(mongodbURI)
 
 const yayanimesProvider = new YayanimesProvider()
 const logger = new Logger(config)
 
-const listAnimeUseCase = new ListAnimeUseCase(yayanimesProvider, logger)
+const listAnimeUseCase = new ListAnimeUseCase(
+  animeRepository,
+  yayanimesProvider,
+  logger
+)
+
 const listAnimeController = new ListAnimeController(listAnimeUseCase)
 
 export { listAnimeController, listAnimeUseCase }
