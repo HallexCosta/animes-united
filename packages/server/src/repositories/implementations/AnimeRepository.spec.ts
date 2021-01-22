7787844c5051565cbba7aab41fcdf85ad49a5f18
HallexCosta <hallex.costa@hotmail.com>
2021-01-21 23:50:08 -0300

    Change throw error to undefined if not find anime by name

--- packages/server/src/repositories/implementations/AnimeRepository.spec.ts
+++ packages/server/src/repositories/implementations/AnimeRepository.spec.ts
@@ -16,7 +16,7 @@ const anime: Anime = {
   status: 'Completo',
   yearRelease: 2015,
   rating: 9.6,
-  sinopse:
+  synopsis:
     'A história gira em torno de habilidades especiais que ocorrem entre um pequeno percentual de meninos e meninas na puberdade. Yuu Otosaka usa seu poder sem os outros saberem, e vive uma vida escolar bastante normal. Perante ele, de repente, aparece uma menina, Nao Tomori. Devido ao seu encontro com ela, o destino dos usuários de poderes especiais será exposto.',
   streamings: {
     episodes: [
@@ -140,19 +140,14 @@ describe('Test Anime Repository', () => {
   )
 
   it(
-    'Should be able to throw error if not find anime by name',
+    'Should be able to return undefined if not find anime by name',
     async done => {
       const animeRepository = new AnimeRepository(mongodbURITest)
       const animeName = 'QUALQUER NOME'
-      let error
-      try {
-        await animeRepository.findByName(animeName)
-      } catch (e) {
-        error = e
-      }
 
-      expect(error.name).toBe('Error')
-      expect(error.message).toBe(`Anime with name "${animeName}" not found`)
+      const received = await animeRepository.findByName(animeName)
+
+      expect(received).toBe(undefined)
       done()
     },
     timeout
