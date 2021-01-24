a672a094b8bd50f6daa720fc32954406ba2a8460
HallexCosta <hallex.costa@hotmail.com>
2021-01-24 02:00:01 -0300

    Add entity Episode

---
+++ packages/server/src/entities/Episode.ts
@@ -0,0 +1,21 @@
+import { guid } from '@animes-united/hash-generator'
+
+export class Episode {
+  public readonly id?: string
+
+  public readonly title: string
+  public readonly number: number
+  public readonly thumbnail: string
+  public readonly qualityStreaming: string
+  public readonly url: string
+
+  constructor(props: Omit<Episode, 'id'>, id?: string) {
+    Object.assign(this, props)
+
+    if (!id) {
+      this.id = guid()
+    }
+
+    Object.freeze(this)
+  }
+}
