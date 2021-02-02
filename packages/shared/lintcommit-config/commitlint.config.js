3f56a890e1b1c1996ae40abd9af26011046027e5
HallexCosta <hallex.costa@hotmail.com>
2021-02-02 14:36:15 -0300

    feat(shared): add new rules for scope-enum of commitlint

--- packages/shared/lintcommit-config/commitlint.config.js
+++ packages/shared/lintcommit-config/commitlint.config.js
@@ -1,3 +1,23 @@
+const { join } = require('path')
+const { readdirSync: readDirectory } = require('fs')
+const DEFAULT_SCOPES = ['repo']
+
+const packageDirNames = readDirectory(
+  join(__dirname, '..', '..', '..', 'packages'),
+  { withFileTypes: true }
+)
+  .filter(entry => entry.isDirectory())
+  .map(dir => dir.name)
+
+const scopes = DEFAULT_SCOPES.concat(packageDirNames)
+
 module.exports = {
-  extends: ['monorepo']
+  extends: ['monorepo'],
+  rules: {
+    'scope-enum': [
+      2, // throw error
+      'always',
+      scopes
+    ]
+  }
 }
