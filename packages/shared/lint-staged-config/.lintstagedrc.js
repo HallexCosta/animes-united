73bbfff9f4d47e521187220a1fc7c25ea1e643bc
HallexCosta <hallex.costa@hotmail.com>
2021-02-07 15:00:01 -0300

    chore(lint-staged-config): remove bash script from lint-staged tasks
    Signed-off-by: HallexCosta <hallex.costa@hotmail.com>

--- packages/shared/lint-staged-config/.lintstagedrc.js
+++ packages/shared/lint-staged-config/.lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports =  {
   '*.ts': [
-    './packages/shared/lint-staged-config/staged-files-tests.sh',
     'eslint --fix',
-    'yarn test $STAGED_FILES_TEST --bail --findRelatedTests'
+    'yarn test --bail --findRelatedTests'
   ]
 }
