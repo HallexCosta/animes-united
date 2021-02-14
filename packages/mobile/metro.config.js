67cde41d0d947fb7ac651e3b7db6f15cf33dfeef
HallexCosta <hallex.costa@hotmail.com>
2021-02-14 15:48:15 -0300

    feat(mobile): add support for SVG files

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,3 +1,22 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces')
+const { getDefaultConfig } = require('metro-config')
+const configuration = createMetroConfiguration(__dirname)
 
-module.exports = createMetroConfiguration(__dirname)
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig()
+
+  return {
+    ...configuration,
+    transformer: {
+      ...configuration.transformer,
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
+    resolver: {
+      ...configuration.resolver,
+      assetExts: configuration.resolver.assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  }
+})()
