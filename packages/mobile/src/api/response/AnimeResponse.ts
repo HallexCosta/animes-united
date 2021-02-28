42a5b5c0ae2065a105d2c01262a7c929c48decaa
HallexCosta <hallex.costa@hotmail.com>
2021-02-28 18:04:34 -0300

    feat(mobile): convert types (EpisodeResponse, StreamingsResponse, AnimeResponse) to class

--- packages/mobile/src/api/response/AnimeResponse.ts
+++ packages/mobile/src/api/response/AnimeResponse.ts
@@ -1,25 +1,54 @@
-export type EpisodeResponse = {
-  title: string
-  number: number
-  thumbnail: string
-  qualityStreaming: string
-  url: string
+import { guid } from '@animes-united/hash-generator'
+
+export class EpisodeResponse {
+  public readonly id: string
+
+  public readonly title: string
+  public readonly number: number
+  public readonly thumbnail: string
+  public readonly qualityStreaming: string
+  public readonly url: string
+
+  public constructor(props: Omit<EpisodeResponse, 'id'>, id?: string) {
+    Object.assign(this, props)
+
+    if (!id) {
+      this.id = guid()
+    }
+
+    Object.freeze(props)
+  }
 }
 
-export type StreamingsResponse = {
-  episodes: EpisodeResponse[]
-  ovas: EpisodeResponse[]
+export class StreamingsResponse {
+  public readonly episodes: EpisodeResponse[]
+  public readonly ovas: EpisodeResponse[]
+
+  public constructor(props: StreamingsResponse) {
+    Object.assign(this, props)
+    Object.freeze(this)
+  }
 }
 
-export type AnimeResponse = {
-  _id: string
-  name: string
-  imageURL: string
-  genre: string
-  status: string
-  studio: string
-  synopsis: string
-  yearRelease: number
-  rating: number
-  streamings: StreamingsResponse
+export class AnimeResponse {
+  public readonly _id: string
+  public readonly name: string
+  public readonly imageURL: string
+  public readonly genre: string
+  public readonly status: string
+  public readonly studio: string
+  public readonly synopsis: string
+  public readonly yearRelease: number
+  public readonly rating: number
+  public readonly streamings: StreamingsResponse
+
+  public constructor(props: Omit<AnimeResponse, '_id'>, id?: string) {
+    Object.assign(props, this)
+
+    if (!id) {
+      this._id = guid()
+    }
+
+    Object.freeze(this)
+  }
 }
