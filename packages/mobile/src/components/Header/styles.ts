3ebb0ae0a0e82942697eb0f659c57d93451103b0
HallexCosta <hallex.costa@hotmail.com>
2021-04-18 15:42:23 -0300

    chore(mobile): add category word on the side button back

--- packages/mobile/src/components/Header/styles.ts
+++ packages/mobile/src/components/Header/styles.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 
   margin-right: 10px;
 
-  /* margin-top: 16px; */
+  margin-top: 16px;
 `
 
 export const Button = styled.TouchableOpacity``
