723760c54bdd7647ce08f9f67e6ceaa92a9ac620
HallexCosta <hallex.costa@hotmail.com>
2021-04-27 15:44:52 -0300

    chore(mobile): add ref, source, rate, volume, shouldPlay configs

--- packages/mobile/src/screens/WatchEpisode/index.tsx
+++ packages/mobile/src/screens/WatchEpisode/index.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
-import VideoPlay, { VideoProperties } from 'react-native-video'
+import React, { useRef } from 'react'
+import { LayoutChangeEvent } from 'react-native'
+
+import { Container, Video } from './styles'
 
 type WatchEpisodeProps = {
   url: string
 }
-
 export function WatchEpisode({ url: uri }: WatchEpisodeProps) {
-  const [options] = useState<VideoProperties>({
-    source: {
-      uri: 'background'
-    }
-  })
+  const videoRef = useRef(null)
 
-  return <VideoPlay {...options} />
+  function onLayout(event: LayoutChangeEvent) {
+    event.preventDefault()
+  }
+
+  return (
+    <Container>
+      <Video
+        ref={videoRef}
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'
+        }}
+        rate={1.0}
+        volume={0.1}
+        isMuted={false}
+        useNativeControls={true}
+        shouldPlay={true}
+        resizeMode="contain"
+        onLayout={onLayout}
+      />
+    </Container>
+  )
 }
