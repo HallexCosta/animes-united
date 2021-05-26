eb32ab159a681dd7d5b46607c3e711cbe5a9bea0
HallexCosta <hallex.costa@hotmail.com>
2021-05-26 14:18:26 -0300

    chore(server): add function to get ip address

--- packages/server/src/common/configs/puppeteer.ts
+++ packages/server/src/common/configs/puppeteer.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import dotenv from 'dotenv'
+import { execSync } from 'child_process'
 dotenv.config({ path: path.join(__dirname, '..', '..', '..', '.env') })
 
 type PuppeteerLaunchConfig = {
@@ -15,6 +16,17 @@ const launch: PuppeteerLaunchConfig = {
   executablePath,
   headless: !debug,
   args: ['--no-sandbox']
+}
+
+export function getIPv4(): string {
+  const ifconfig = execSync('ifconfig eth0 | grep inet').toString()
+  const regex = /(?:[0-9]{1,3}\.){3}[0-9]{1,3}/
+
+  if (!regex.test(ifconfig)) throw new Error('IPV4: no found valid ip address')
+
+  const ipMatch = ifconfig.match(regex)
+  const ip = ipMatch ? ipMatch[0].trim() : '127.0.0.1'
+  return ip
 }
 
 export { launch }
