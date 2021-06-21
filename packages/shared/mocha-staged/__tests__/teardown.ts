dc347fc9fa1729df207a087aafdb77cfd148ecf5
HallexCosta <hallex.costa@hotmail.com>
2021-06-21 00:44:32 -0300

    chore(mocha-staged): move moveSpecFileOutsideToStageArea

--- packages/shared/mocha-staged/__tests__/teardown.ts
+++ packages/shared/mocha-staged/__tests__/teardown.ts
@@ -1,17 +1,9 @@
-import { unlink } from 'fs/promises'
-
-async function deleteSpecFile(filepath: string) {
-  try {
-    await unlink(filepath)
-  } catch (e) {
-    console.log(e)
-  }
-}
+import { moveSpecFileOutsideToStageArea, deleteSpecFile } from './functions'
 
 async function teardown() {
   const filepath = `${__dirname}/unit/testing.spec.ts`
+  await moveSpecFileOutsideToStageArea(filepath)
   await deleteSpecFile(filepath)
-  console.log('Finish Teardown')
 }
 
 after(teardown)
