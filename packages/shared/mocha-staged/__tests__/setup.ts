4374ffabcb0568a52c5113b79720245c224da0bb
HallexCosta <hallex.costa@hotmail.com>
2021-06-21 00:43:17 -0300

    chore(mocha-staged): move createSpecFile to functions file

--- packages/shared/mocha-staged/__tests__/setup.ts
+++ packages/shared/mocha-staged/__tests__/setup.ts
@@ -1,16 +1,9 @@
-import { open } from 'fs/promises'
-
-async function createSpecFile(filepath: string) {
-  try {
-    await open(filepath, 'a')
-  } catch (e) {
-    console.log(e)
-  }
-}
+import { moveSpecFileToStageArea, createSpecFile } from './functions'
 
 async function setup() {
   const filepath = `${__dirname}/unit/testing.spec.ts`
   await createSpecFile(filepath)
+  await moveSpecFileToStageArea(filepath)
 }
 
 before(setup)
