37b958100e28513dab84317d63eabfebf72b16ed
HallexCosta <hallex.costa@hotmail.com>
2021-06-21 21:18:21 -0300

    chore(mocha-staged): create func runTests and types
    Create SimpleStageFile and StageFile types

--- packages/shared/mocha-staged/src/git-staged-area.ts
+++ packages/shared/mocha-staged/src/git-staged-area.ts
@@ -1,5 +1,15 @@
 import { execSync } from 'child_process'
 
+export type SimpleStageFile = {
+  scope: string
+  file: string
+}
+
+export type StageFile = {
+  scope: string
+  files: string[]
+}
+
 export function getGitStagedAreaFiles(regex = '\\.spec\\.ts$'): string[] {
   const specs = execSync(
     `git diff --cached --name-only --diff-filter=ACM | grep ${regex}`
@@ -13,7 +23,37 @@ export function getGitStagedAreaFiles(regex = '\\.spec\\.ts$'): string[] {
 }
 
 export function getStagedFiles(
-  preapareStagedFiles: (specFile: string) => string
-): string {
-  return getGitStagedAreaFiles().map(preapareStagedFiles).join(' ')
+  preapareStagedFiles: (specFile: string) => SimpleStageFile
+): SimpleStageFile[] {
+  return getGitStagedAreaFiles().map(preapareStagedFiles)
+}
+
+export function separateByScope(stages: SimpleStageFile[]): StageFile[] {
+  const stagedFiles: StageFile[] = []
+
+  for (const { scope, file } of stages) {
+    const alreadyScope = stagedFiles.find(staged => staged.scope === scope)
+
+    if (alreadyScope) {
+      stagedFiles.map(staged => staged.files.push(file))
+    } else {
+      const files = []
+      files.push(file)
+
+      stagedFiles.push({
+        scope,
+        files
+      })
+    }
+  }
+
+  return stagedFiles
+}
+
+export function runTests(stageFiles: StageFile[]): boolean {
+  for (const { scope, files } of stageFiles) {
+    const specs = files.join(' ')
+    // execSync(`yarn workspace @animes-united/${scope} test ${specs}`)
+  }
+  return true
 }
