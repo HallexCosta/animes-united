545d739db133342b9582990d31d5dce060024378
HallexCosta <hallex.costa@hotmail.com>
2021-07-02 03:02:41 -0300

    feat(server): add UpdateAnimeController

---
+++ packages/server/src/controllers/UpdateAnimeController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { UpdateAnimeService } from 'src/services/UpdateAnimeService'
+
+export class UpdateAnimeController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, category } = request.params
+
+    const updateAnimeService = new UpdateAnimeService()
+
+    const anime = await updateAnimeService.execute({
+      name,
+      category
+    })
+
+    return response.json(anime)
+  }
+}
