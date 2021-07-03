9165bcb3e3b9a6785d4a78a5c68be95e15c49e28
HallexCosta <hallex.costa@hotmail.com>
2021-07-03 01:20:53 -0300

    chore(server): use middleware handleErrors

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,14 @@
 import path from 'path'
-import express, { Application } from 'express'
+import express from 'express'
+
 import { routes } from './routes'
 import { logger } from './logger'
 
-const app: Application = express()
+import { handleErrors } from '@middlewares'
+
+import 'express-async-errors'
+
+const app = express()
 
 logger()
   .then()
@@ -11,6 +16,7 @@ logger()
 
 app.use(express.json())
 app.use(routes)
+app.use(handleErrors)
 app.use('/static', express.static(path.join(__dirname, 'uploads')))
 
 export { app }
