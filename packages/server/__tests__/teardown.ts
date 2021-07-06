93980d18e36da3e03cec325847cc780a2be66603
HallexCosta <hallex.costa@hotmail.com>
2021-07-06 14:00:00 -0300

    refactor(server): add func to disconnect from test database

--- packages/server/__tests__/teardown.ts
+++ packages/server/__tests__/teardown.ts
@@ -2,15 +2,19 @@ import { MongoClient, Db } from 'mongodb'
 import { mongodbURITest } from '@common/configs/mongodb'
 
 let cachedDb: Db
+let mongoClient: MongoClient
 
-async function _prepareConnection() {
+async function connection() {
   if (cachedDb) {
     return cachedDb
   }
+
   const client = await MongoClient.connect(mongodbURITest, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+
+  mongoClient = client
 
   const url = new URL(mongodbURITest)
   const dbName = url.pathname.substr(1)
@@ -21,19 +25,30 @@ async function _prepareConnection() {
 
   return db
 }
-async function connection() {
-  const db = await _prepareConnection()
+
+async function dropCollections() {
+  const db = await connection()
   const drops = []
+
   for (const collection of await db.collections()) {
     drops.push(await db.collection(collection.collectionName).drop())
   }
+
   const fails = !!drops.find(drop => drop === false)
   const dropped = !fails
+
   console.log('Teardown: Database for test dropped', dropped)
+}
+
+async function disconnect() {
+  await mongoClient.close()
 }
 
 async function teardown() {
   await connection()
+  await dropCollections()
+  await disconnect()
+
   console.log('Finish Teardown')
 }
 
