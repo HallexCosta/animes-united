99371e2ba11860d4852256a163098077e501e3a3
HallexCosta <hallex.costa@hotmail.com>
2021-07-09 02:58:40 -0300

    chore(server): add PartialBy and check for _id not is undefined

--- packages/server/src/entities/Anime.ts
+++ packages/server/src/entities/Anime.ts
@@ -2,8 +2,11 @@ import { guid } from '@animes-united/hash-generator'
 import { Episode } from '.'
 import { Streamings } from './Streamings'
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
 export class Anime {
-  public readonly _id?: string
+  public readonly _id: string
 
   public readonly name: string
   public readonly image_url: string
@@ -19,7 +22,7 @@ export class Anime {
   public readonly created_at: Date
   public readonly updated_at: Date
 
-  constructor(props: Anime) {
+  constructor(props: PartialBy<Anime, '_id'>) {
     Object.assign(this, {
       ...props,
       streamings: new Streamings({
@@ -30,7 +33,7 @@ export class Anime {
       })
     })
 
-    if (!this._id) {
+    if (!props._id) {
       this._id = guid()
     }
 
