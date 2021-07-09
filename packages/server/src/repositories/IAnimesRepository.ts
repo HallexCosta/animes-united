d1c9752e923e6a17e37c3e1f8b55a3e835e47ea0
HallexCosta <hallex.costa@hotmail.com>
2021-07-09 03:02:09 -0300

    refactor(server): add new types and methods to IAnimesRepository

--- packages/server/src/repositories/IAnimesRepository.ts
+++ packages/server/src/repositories/IAnimesRepository.ts
@@ -1,14 +1,29 @@
-import { ObjectId } from 'mongodb'
-import { Anime } from '@entities/Anime'
-import { CategoryAnime } from '@repository'
+import { Anime } from '@entities'
+
+export type AnimeCategory = {
+  animes: Anime[]
+  category: string
+}
+
+export type UpdatedAnime = {
+  updatedCount: number
+}
+
+export type DeletedAnime = {
+  deletedCount: number
+}
 
 export interface IAnimesRepository {
-  findAll(): Promise<CategoryAnime[]>
-  findByCategory(category?: string): Promise<CategoryAnime>
   category(category: string): IAnimesRepository
-  save(anime: Anime): Promise<boolean>
-  updateById(anime: Omit<Anime, '_id'>, _id: ObjectId): Promise<boolean>
-  updateByName(anime: Omit<Anime, '_id'>, name: string): Promise<boolean>
-  findByName(name: string): Promise<Anime | undefined>
-  deleteByName(name: string): Promise<boolean>
+  create(anime: Omit<Anime, '_id' | 'created_at' | 'updated_at'>): Anime
+  findAll(): Promise<AnimeCategory[]>
+  findByCategory(category?: string): Promise<Anime[]>
+  findById(_id: string): Promise<Anime | null>
+  findByName(name: string): Promise<Anime | null>
+  updateById(_id: string, anime: Anime): Promise<UpdatedAnime>
+  updateByName(name: string, anime: Anime): Promise<UpdatedAnime>
+  deleteById(_id: string): Promise<DeletedAnime>
+  deleteByName(name: string): Promise<DeletedAnime>
+  save(anime: Anime): Promise<Anime>
+  saveMany(animes: Anime[]): Promise<Anime[]>
 }
